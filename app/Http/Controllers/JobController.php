1d9aa8f97e9dcd3b5d9d0397d88033817e68c69b
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-26 06:25:24 +0200

    added the shifts now showing correctly

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -47,22 +47,29 @@ class JobController extends Controller
     //view the jobs on the client side by their id
     public function viewJobById($id){
         
-        $job = Job::find($id)->id;
-        $today = Carbon::today();
-        $endDate = $today->copy()->addDays(7);
-        $jobsWithDetails = Job::join('job_details', 'jobs.id', '=', 'job_details.job_id')
-        ->select(
-            'jobs.job as job',
-            'job_details.date as date',
-            'job_details.num_people as people',
-            'job_details.shift as shift',
-        )
-        ->where('job_details.job_id', '=', $job)
-        ->where('job_details.date', '>=', $today)
-        ->where('job_details.date', '<=', $endDate)
-        ->get();
-        return view('viewJobById')->with('jobsWithDetails', $jobsWithDetails);
+        $job = Job::find($id);
+    
+        if (!$job) {
+           echo "no jobs found";
+        }
+        
+          $today = Carbon::today();
+          $endDate = $today->copy()->addDays(7);
+        
+          $jobsWithDetails = Job::leftJoin('job_details', 'jobs.id', '=', 'job_details.job_id')
+            ->select(
+                'jobs.job as job',
+                'job_details.date as date',
+                'job_details.num_people as people',
+                'job_details.shift as shift'
+            )
+            ->where('jobs.id', '=', $job->id) // Use $job->id here to access the job ID.
+            ->where('job_details.date', '>=', $today)
+            ->where('job_details.date', '<=', $endDate)
+            ->get();
 
+        return view('viewJobById')->with('jobsWithDetails', $jobsWithDetails);
+    
     }
 
     /**
