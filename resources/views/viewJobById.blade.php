35272acd6845a75ad50dc4ea2c1c65a6beb55e81
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-26 07:36:33 +0200

    entries are now displaying in a dynamic way

--- resources/views/viewJobById.blade.php
+++ resources/views/viewJobById.blade.php
@@ -1,10 +1,18 @@
-@extends('layouts.app') <!-- Use your layout file if you have one -->
+@extends('layouts.app')
 @section('content')
-@foreach($jobsWithDetails as $jobDetail)
-    Date: {{ $jobDetail->date }}
-    Morning Jobs: {{ $jobDetail->morning_jobs }}
-    Night Jobs: {{ $jobDetail->night_jobs }}
-    Late Jobs: {{ $jobDetail->late_jobs }}
-    Long Jobs: {{ $jobDetail->long_jobs }}
-@endforeach
+<div class="row">
+    @foreach($jobsWithDetails as $jobDetail)
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Date: {{ $jobDetail->date }}</h5>
+                    <p class="card-text">Morning {{ $jobDetail->morning_jobs }}</p>
+                    <p class="card-text">Night  {{ $jobDetail->night_jobs }}</p>
+                    <p class="card-text">Late  {{ $jobDetail->late_jobs }}</p>
+                    <p class="card-text">Long  {{ $jobDetail->long_jobs }}</p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
 @endsection
