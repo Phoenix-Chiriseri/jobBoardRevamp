85dee8b43f281104b201d55b23d28cfe0beee6bd
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-27 14:29:59 +0200

    added label to show if there is a job or not

--- resources/views/viewJobById.blade.php
+++ resources/views/viewJobById.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    @foreach ($jobsWithDetails->groupBy('date') as $date => $records)
-        <div class="card mb-4">
-            <div class="card-header">
-                <h5 class="card-title">Date: {{ $date }}</h5>
+    @if ($jobsWithDetails->isEmpty())
+        <p>No job records found.</p>
+    @else
+        @foreach ($jobsWithDetails->groupBy('date') as $date => $records)
+            <div class="card mb-4">
+                <div class="card-header">
+                    <h5 class="card-title">Date: {{ $date }}</h5>
+                </div>
+                <div class="card-body">
+                    @foreach ($records as $record)
+                        <h6 class="card-subtitle mb-2 text-muted">{{ $record->job }}</h6>
+                        <p class="card-text">Shift: {{ $record->shift }}</p>
+                        <p class="card-text">Total Number of People: {{ $record->total_num_people }}</p>
+                    @endforeach
+                </div>
             </div>
-            <div class="card-body">
-                @foreach ($records as $record)
-                    <h6 class="card-subtitle mb-2 text-muted">{{ $record->job }}</h6>
-                    <p class="card-text">Shift: {{ $record->shift }}</p>
-                    <p class="card-text">Total Number of People: {{ $record->total_num_people }}</p>
-                @endforeach
-            </div>
-        </div>
-    @endforeach
+        @endforeach
+    @endif
 </div>
 @endsection
